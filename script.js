e82d13a812b19a4c4b255868a91cbc7fe783d0b5
Mohan-krishna808 <kandikuntamohan3@gmail.com>
2025-10-17 03:03:39 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text", {
-  strings: ["Frontend Developer", "Backend Developer", "Full Stack Developer"],
-  typeseed: 300,
-  backspeed: 100,
-  backDelay: 1000,
-  loop: true,
-});
