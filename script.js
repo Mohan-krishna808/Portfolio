201fb151e67506fe98e38dbde51d22712b21e665
Mohan-krishna808 <kandikuntamohan3@gmail.com>
2025-10-17 02:45:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Backend Developer", "Full Stack Developer"],
+  typeseed: 300,
+  backspeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
